d87de09785b1f9b159987b7080844ad7f69effcb
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-03-26 00:42:46 -0300

    incorpora primer test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,3 +1,29 @@
+/// <reference types="Cypress" />
+
+const URL = "127.0.0.1:8080";
+
+context('Calculador de Familiares', () => {
+  before( () => {
+    cy.visit(URL);
+  });
+
+  describe('verifica carga de la pagina', () => {
+
+    it('carga el titulo', () => {
+      cy.get('h1').contains('Calculador de Edades y Sueldos de Familiares');
+    })
+    
+    it('carga el navbar', () => {
+      cy.get('nav').should('have.length',1);
+    })
+    
+    it('carga el formulario', () => {
+      cy.get('button').should('have.length',1);
+      cy.get('input').should('have.length',1);
+    })
+
+  })
+})
 
 
 
